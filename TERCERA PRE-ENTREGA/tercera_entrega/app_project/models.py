ec36d5e3c456a2bd0fb6a106f03342258011bc72
valenmarinozzi <131627971+valenmarinozzi@users.noreply.github.com>
2024-02-12 14:28:35 -0300

    Add files via upload

---
+++ TERCERA PRE-ENTREGA/tercera_entrega/app_project/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# Create your models here.
+
+class Curso(models.Model):
+    nombre = models.CharField(max_length=40)
+    camada = models.IntegerField()
+
+class Curso(models.Model):
+    nombre = models.CharField(max_length=40)
+    apellido = models.CharField(max_length=40)
+    email = models.EmailField()
+
+class Profesor(models.Model):
+    nombre = models.CharField(max_length=40)
+    apellido = models.CharField(max_length=40)
+    email = models.EmailField()
+    profesion = models.CharField(max_length=30)
+
+class Entregable(models.Model):
+    nombre = models.CharField(max_length=40)
+    fecha_de_entrega = models.DateField()
+    entregado = models.BooleanField()
